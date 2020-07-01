07f30bd03f07a82bad6274d5654f78f4ca5af28a
kengaku <keng9358@gmail.com>
2020-07-02 04:58:18 +0900

    docker-compose install

--- centos8/docker.sh
+++ centos8/docker.sh
@@ -9,4 +9,8 @@ usermod -aG docker $USER
 newgrp docker
 docker version
 
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
 docker run -d -p 80:80 nginx
