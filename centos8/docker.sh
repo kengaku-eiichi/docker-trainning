4c12eff8d4e984da4fa598fa51ff481d63a6b885
kengaku <keng9358@gmail.com>
2020-07-03 22:48:16 +0900

    Dockerfile作成

--- centos8/docker.sh
+++ centos8/docker.sh
@@ -13,6 +13,8 @@ curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compos
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-#docker build . -t trainning/nginx
 docker run -d -p 80:80 --rm -v /git/docker-trainning/html:/usr/share/nginx/html -v /git/docker-trainning/log:/var/log nginx:1.19
-#docker exec -it xxx /bin/bash
+# docker exec -it xxx /bin/bash
+
+# docker build -t trainning/nginx /git/docker-trainning
+# docker run -d -p 80:80 --rm -v /git/docker-trainning/log:/var/log trainning/nginx
